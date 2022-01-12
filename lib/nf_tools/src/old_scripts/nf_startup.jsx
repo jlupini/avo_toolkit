84ea3dfbeb8772de2eb9ef948758bd8cdf0520fa
Jesse Lupini <jlupini@gmail.com>
2022-01-12 13:43:15 -0800

    fix nf_startup path to eval'd files

--- lib/nf_tools/src/old_scripts/nf_startup.jsx
+++ lib/nf_tools/src/old_scripts/nf_startup.jsx
@@ -1,2 +1,2 @@
-$.evalFile("nf_InstallPseudoEffects.jsx");
-$.evalFile("runtimeLibraries.jsx");
+$.evalFile(File($.fileName).path + "/nf_InstallPseudoEffects.jsx");
+$.evalFile(File($.fileName).path + "/runtimeLibraries.jsx");
